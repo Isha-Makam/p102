4ec4a856898f2c5e9ffc7050469216d4c25e89a9
Isha Makam <117507150+Isha-Makam@users.noreply.github.com>
2024-07-08 15:15:45 +0400

    Add files via upload

---
+++ 102_project.py
@@ -0,0 +1,32 @@
+import os
+import shutil
+
+from_dir = "/Users/ishamakam/Downloads/folder"
+to_dir = "/Users/ishamakam/Desktop"
+
+list_of_files = os.listdir(from_dir)
+#print(list_of_files)
+
+for file_name in list_of_files:
+    name,extension = os.path.splitext(file_name)
+    #print(name)
+    #print(extension)
+
+    if extension == "":
+        continue
+    if extension in ['.pdf','.doc','.ppt','.docs',]:
+
+        path1 = from_dir + '/' + file_name
+        print(path1)
+        path2 = to_dir + '/' + 'document files'
+        path3 = to_dir + '/' + 'document files' + '/' + file_name
+
+        if os.path.exists(path2):
+            print("moving file...",file_name)
+            shutil.move(path1,path3)
+        
+        else:
+            os.makedirs(path2)
+            print("creating directory...")
+            print("moving file...",file_name)
+            shutil.move(path1,path3)
